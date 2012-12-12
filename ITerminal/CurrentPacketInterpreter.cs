15189fb12981ab9709cf3d674e583bffa5412cf2
George Hahn <@George_Hahn>
2012-12-12 17:33:42 -0500

    Packet interpreter DI

---
+++ ITerminal/CurrentPacketInterpreter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace Terminal_Interface
+{
+    public class CurrentPacketInterpreter : INotifyPropertyChanged
+    {
+        public CurrentPacketInterpreter(IPacketInterpreter packetInterpreter)
+        {
+            PacketInterpreter = packetInterpreter;
+        }
+
+        public IPacketInterpreter PacketInterpreter { get; private set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
