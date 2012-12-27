a5fe02c38047bbf258f513b6f0cbb50ce89f9cfc
GeorgeHahn <@George_Hahn>
2012-12-27 18:28:02 -0500

    Fix a number of bugs

--- PacketParser/StringPacketInterpreter.cs
+++ PacketParser/StringPacketInterpreter.cs
@@ -13,16 +13,16 @@ namespace PacketParser
         {
             var hex = new StringBuilder(packet.Length * 4);
             int endIndex = packet.Length - 1;
-            while ((packet[endIndex] == 0) && (endIndex >= 0))
+            while ((endIndex >= 0) && (packet[endIndex] == 0))
                 endIndex--;
 
             if (endIndex < 0)
-                return "Empty string\r\n";
+                return string.Empty;
 
             for (int index = 0; index <= endIndex; index++)
             {
                 var b = packet[index];
-                if ((b >= 0x21) && (b <= 0x7E))
+                if ((b >= 0x20) && (b <= 0x7E))
                     hex.Append(Encoding.UTF8.GetChars(new []{ b }));
                 else
                     hex.AppendFormat("[{0:X2}]", b);
